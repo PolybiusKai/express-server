3ef0f1b3f54c3b96922fc780133bef4a3496e1dc
Rob Smith <legendary.belief@gmail.com>
2023-08-04 20:28:02 -0700

    Added routes

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const path = require("path");
+app.use(express.static("public"));
+
 const PORT = 5500;
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "contact.html"));
 });
